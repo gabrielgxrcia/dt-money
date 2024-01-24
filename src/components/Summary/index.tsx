fc04b27588bb498a5b1ccaff59064f733e87f188
Gabriel <gabrielgrazeffi123@gmail.com>
2024-01-24 16:07:08 -0300

    feat: add summary components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,12 @@
+import { css } from '../../../styled-system/css'
+import { SummaryCard, SummaryCardLeave, SummaryCardTotal } from './components/SummaryCard'
+
+export function Summary () {
+  return (
+    <section className={css({ width: '100%', maxWidth: '1120px', m: '0 auto', p: '0 1.5rem', display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '2rem', mt: '-5rem'})}>
+      <SummaryCard />
+      <SummaryCardLeave />
+      <SummaryCardTotal variant='green'/>
+    </section>
+  )
+}
