144b58001d27ed5dbca5ce137f76733c937f1aaa
Gabriel <gabrielgrazeffi123@gmail.com>
2024-01-24 19:48:58 -0300

    refactor: add style in checked state in radiogroup

--- src/components/NewTransactionModal/TransactionTypeRadio.tsx
+++ src/components/NewTransactionModal/TransactionTypeRadio.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { css } from "../../../styled-system/css";
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
@@ -8,9 +9,34 @@ export interface TransactionTypeButtonProps {
 
 export const TransactionTypeRadio: React.FC<TransactionTypeButtonProps> = ({ variant, children }) => {
   const color = variant === 'income' ? '#00B37E' : '#F75A68';
+  const checkedColor = variant === 'income' ? '#00875F' : '#AB222E';
+
+  const childrenWithProps = React.Children.map(children, (child, index) => {
+
+    if (React.isValidElement(child) && child.type === 'svg' && index === 0) {
+      return React.cloneElement(child as React.ReactElement<React.SVGProps<SVGSVGElement>>, { fill: '#fff' });
+    }
+    return child;
+  });
+
   return (
-    <RadioGroup.Item value={variant} className={css({ background: '#29292E', p: '1rem', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5rem', borderRadius: '6px', cursor: 'pointer', border: '0', color })}>
-      {children}
+    <RadioGroup.Item value={variant} className={css({ 
+      background: '#29292E', 
+      p: '1rem', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      gap: '0.5rem', 
+      borderRadius: '6px', 
+      cursor: 'pointer', 
+      border: '0', 
+      color,
+      '&[data-state="checked"]': {
+        color: '#fff',
+        background: checkedColor,
+      },
+    })}>
+      {childrenWithProps}
     </RadioGroup.Item>
   )
 }
