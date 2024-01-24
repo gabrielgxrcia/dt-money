fa0cc4bf1c84830665ad3172f3f0ff225ce8e1d8
Gabriel <gabrielgrazeffi123@gmail.com>
2024-01-24 16:38:47 -0300

    feat: table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { PriceHighLight, mainStyle, tableStyle, tdStyle } from './styles'
 import { Header } from '../../components/Header/Header'
 import { Summary } from '../../components/Summary'
 
@@ -6,6 +7,33 @@ export function Transactions () {
     <div>
       <Header />
       <Summary />
+
+    <main className={mainStyle}>
+      <table className={tableStyle}>
+        <tbody>
+          <tr>
+            <td className={tdStyle}>Desenvolvimento de site</td>
+            <td className={tdStyle}>
+              <PriceHighLight variant="income">
+                R$ 12.000,00
+                </PriceHighLight>
+            </td>
+            <td className={tdStyle}>Venda</td>
+            <td className={tdStyle}>24/01/2024</td>
+          </tr>
+          <tr>
+          <td className={tdStyle}>Hamburguer</td>
+          <td className={tdStyle}>
+               <PriceHighLight variant="outcome">
+                - R$ 59,00
+                </PriceHighLight>
+          </td>
+          <td className={tdStyle}>Alimentação</td>
+          <td className={tdStyle}>22/01/2024</td>
+          </tr>
+        </tbody>
+      </table>
+      </main>
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-refresh/only-export-components */
+import { css } from "../../../styled-system/css";
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome';
+  children?: React.ReactNode;
+}
+
+export const mainStyle = css({ width: '100%', maxW: '1120px', m: '4rem auto 0', p: '0 1.5rem'});
+export const tableStyle =  css({ width: '100%', borderCollapse: 'separate', borderSpacing: '0 0.5rem'});
+export const tdStyle = css({
+  padding: '1.25rem 2rem',
+  background: '#29292E',
+  '&:first-child': {
+    borderTopLeftRadius: '6px',
+    borderBottomLeftRadius: '6px',
+  },
+  '&:last-child': {
+    borderTopRightRadius: '6px',
+    borderBottomRightRadius: '6px',
+  },
+});
+
+export const PriceHighLight: React.FC<PriceHighlightProps> = ({ variant, children }) => {
+  const style = css({
+    color: variant === 'income' ? '#00B37E' : '#F75A68',
+  });
+
+  return <span className={style}>{children}</span>;
+};
