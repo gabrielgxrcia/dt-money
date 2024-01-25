0f1891f40d944139029edc11f2c1b3415130cf21
Gabriel <gabrielgrazeffi123@gmail.com>
2024-01-25 18:16:17 -0300

    feat: create new transactions

--- src/components/NewTransactionModal/NewTransactionModalContainer.tsx
+++ src/components/NewTransactionModal/NewTransactionModalContainer.tsx
@@ -6,25 +6,35 @@ import { TransactionTypeRadio } from './TransactionTypeRadio';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
+import { useTransactions } from '../../pages/Transactions/atoms/transactionAtoms';
 
-const newTransactionFormSchema = z.object({
+const NewTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
   type: z.enum(['income', 'outcome']),
 });
 
-type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
 
 const inputStyled = css({ borderRadius: '6px', border: '0', background: '#121214', color: '#C4C4CC', p: '1rem', '&::placeholder': { color: '#7C7C8A' } });
 
 export function NewTransactionModal() {
-  const { control, register, handleSubmit, formState: { isSubmitting } } = useForm<newTransactionFormInputs>({ resolver: zodResolver(newTransactionFormSchema) });
+  const { control, register, handleSubmit, formState: { isSubmitting }, reset } = useForm<NewTransactionFormInputs>({ resolver: zodResolver(NewTransactionFormSchema) });
+  const { createTransaction } = useTransactions(); 
 
-  async function handleCreateNewTransaction(data: newTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(data);
-  }
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+  const {  description, price, category, type } = data;
+
+  await createTransaction({
+    description,
+    price,
+    category,
+    type,
+  })
+
+  reset()
+}
 
   return (
     <Dialog.Portal>
--- src/pages/Transactions/atoms/transactionAtoms.ts
+++ src/pages/Transactions/atoms/transactionAtoms.ts
@@ -12,12 +12,23 @@ export interface Transaction {
   createdAt: string
 }
 
+interface CreateTransactionInput {
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+}
+
 export const transactionsAtom = atom<Transaction[]>([])
 
 export const loadTransactionsAtom = atom(
   null,
   async (_get, set, query: string = '') => {
-    const response = await api.get('/transactions')
+    const response = await api.get('/transactions', {
+      params: {
+        _sort: '-createdAt',
+      },
+    })
     if (response.data !== null) {
       const transactions = response.data as Transaction[]
       if (query) {
@@ -36,9 +47,23 @@ export function useTransactions() {
   const [transactions, setTransactions] = useAtom(transactionsAtom)
   const [, loadTransactions] = useAtom(loadTransactionsAtom)
 
+  async function createTransaction(data: CreateTransactionInput) {
+    const { description, price, category, type } = data
+
+    const response = await api.post('transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions(state => [response.data, ...state])
+  }
+
   useEffect(() => {
     loadTransactions()
   }, [loadTransactions])
 
-  return { transactions, setTransactions, loadTransactions }
+  return { transactions, setTransactions, loadTransactions, createTransaction }
 }
