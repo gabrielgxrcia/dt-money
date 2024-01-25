1bf222dab5cbce10f1072bc608cfbd7b24976fd9
Gabriel <gabrielgrazeffi123@gmail.com>
2024-01-24 21:38:56 -0300

    feat: add HTTP request to load API transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,8 +2,24 @@ import { PriceHighLight, mainStyle, tableStyle, tdStyle } from './styles'
 import { Header } from '../../components/Header/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
+import { useEffect } from 'react'
+import { useAtom } from 'jotai'
+import { transactionsAtom } from './atoms/transactionAtoms'
 
 export function Transactions () {
+  const [transactions, setTransactions] = useAtom(transactionsAtom)
+
+  useEffect(() => {
+    async function loadTransactions() {
+      const response = await fetch('http://localhost:3333/transactions')
+      const data = await response.json()
+
+     setTransactions(data)
+    }
+
+    loadTransactions()
+  })
+
   return (
     <div>
       <Header />
@@ -14,26 +30,20 @@ export function Transactions () {
 
       <table className={tableStyle}>
         <tbody>
-          <tr>
-            <td className={tdStyle}>Desenvolvimento de site</td>
-            <td className={tdStyle}>
-              <PriceHighLight variant="income">
-                R$ 12.000,00
-                </PriceHighLight>
-            </td>
-            <td className={tdStyle}>Venda</td>
-            <td className={tdStyle}>24/01/2024</td>
-          </tr>
-          <tr>
-          <td className={tdStyle}>Hamburguer</td>
-          <td className={tdStyle}>
-               <PriceHighLight variant="outcome">
-                - R$ 59,00
-                </PriceHighLight>
-          </td>
-          <td className={tdStyle}>Alimentação</td>
-          <td className={tdStyle}>22/01/2024</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+              <td className={tdStyle}>{transaction.description}</td>
+              <td className={tdStyle}>
+                <PriceHighLight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighLight>
+              </td>
+              <td className={tdStyle}>{transaction.category}</td>
+              <td className={tdStyle}>{transaction.createdAt}</td>
+            </tr>
+            )
+          })}
         </tbody>
       </table>
       </main>
